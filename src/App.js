420ffb138e9c92a488c1fb1767647b3d5634c000
VzsoaresDesktopBH <jogolots2@gmail.com>
2022-01-02 20:11:28 -0300

    dolar->real functionality

--- src/App.js
+++ src/App.js
@@ -1,30 +1,96 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { TiArrowRepeat } from "react-icons/ti";
 
 function App() {
+  const [value1, setValue1] = useState("1");
+  const [value2, setValue2] = useState("7");
+  const [moeda1, setMoeda1] = useState("dolar");
+  const [moeda2, setMoeda2] = useState("real");
+  const [lastChanged, setLastChanged] = useState(0);
+  const handleMoedaChange = (e, moeda) => {
+    if (moeda == moeda1) {
+      setMoeda1(e.target.value);
+    } else if (moeda == moeda2) {
+      setMoeda2(e.target.value);
+    }
+  };
+  const applyCambio = () => {
+    if (lastChanged == 0) {
+      setValue2(value1 * 7);
+    }
+    if (lastChanged == 1) {
+      setValue1(value2 / 7);
+    }
+  };
+  const handleInputChange = (e, value) => {
+    if (!Number(e.target.value) && Number(e.target.value) !== 0) {
+      alert("Apenas números são aceitos");
+      return;
+    }
+    if (value == value1) {
+      setValue1(e.target.value);
+      setLastChanged(0);
+    } else if (value == value2) {
+      setValue2(e.target.value);
+      setLastChanged(1);
+    }
+    console.log(value1, value2);
+  };
+  const handleInversion = (e) => {
+    e.preventDefault();
+  };
+  useEffect(() => {
+    applyCambio();
+  }, [value1, value2]);
   return (
     <div className='App'>
       <div className='title-container'>
         <h1>conversor de cambio</h1>
       </div>
       <div className='conversor-container'>
-        <div className='moedas'>
-          <select name='' id=''>
-            <option value='+'>dolar</option>
-            <option value='-'>real</option>
-            <option value='*'>euro</option>
-            <option value='/'>yen</option>
+        <div
+          className='moedas'
+          style={{ display: "flex", justifyContent: "center" }}
+        >
+          <select
+            name=''
+            id='moeda1'
+            onChange={(e) => {
+              console.log("zap");
+              handleMoedaChange(e, moeda1);
+            }}
+          >
+            <option value='dolar'>dolar</option>
+            <option value='real'>real</option>
+            <option value='euro'>euro</option>
+            <option value='yen'>yen</option>
           </select>
+          <div className='div'>
+            <a onClick={handleInversion} href=''>
+              <TiArrowRepeat style={{ fontSize: "2rem" }} />
+            </a>
+          </div>
           <select name='' id=''>
-            <option value='-'>real</option>
-            <option value='+'>dolar</option>
-            <option value='*'>euro</option>
-            <option value='/'>yen</option>
+            <option value='real'>real</option>
+            <option value='dolar'>dolar</option>
+            <option value='euro'>euro</option>
+            <option value='yen'>yen</option>
           </select>
         </div>
         <div className='results'>
-          <input type='text' value={"$1,00"} />
-          <input type='text' value={"$7,00"} />
+          <input
+            type='text'
+            id='value1'
+            onChange={(e) => handleInputChange(e, value1)}
+            value={value1}
+          />
+          <input
+            type='text'
+            id='value2'
+            value={value2}
+            onChange={(e) => handleInputChange(e, value2)}
+          />
         </div>
       </div>
     </div>
