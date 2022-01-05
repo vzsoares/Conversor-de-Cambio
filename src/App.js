361e154be853889345b3dfdbb96dde5a9632e02d
VzsoaresDesktopBH <jogolots2@gmail.com>
2022-01-04 22:01:34 -0300

    ActuallyWorksLOL

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import "./App.css";
 import { TiArrowRepeat } from "react-icons/ti";
 
 function App() {
-  const [value1, setValue1] = useState("1");
-  const [value2, setValue2] = useState("7");
+  const data = {
+    valoresEmDolar: {
+      dolar: 1,
+      real: 0.18,
+      euro: 1.13,
+      yen: 0.0086,
+    },
+    valoresNativos: {
+      dolar: 1,
+      real: 5.68,
+      euro: 0.89,
+      yen: 116.14,
+    },
+  };
+  const [value1, setValue1] = useState(1);
+  const [value2, setValue2] = useState(7);
   const [moeda1, setMoeda1] = useState("dolar");
   const [moeda2, setMoeda2] = useState("real");
   const [lastChanged, setLastChanged] = useState(0);
   const handleMoedaChange = (e, moeda) => {
-    if (moeda == moeda1) {
+    if (moeda == moeda1 && e.target.id == "moeda1") {
       setMoeda1(e.target.value);
     } else if (moeda == moeda2) {
       setMoeda2(e.target.value);
@@ -28,20 +42,33 @@ function App() {
       setValue2(e.target.value);
       setLastChanged(1);
     }
-    console.log(value1, value2);
   };
   const handleInversion = (e) => {
     e.preventDefault();
+    const holdValue1 = moeda1;
+    const holdValue2 = moeda2;
+    setMoeda1(holdValue2);
+    setMoeda2(holdValue1);
+    setValue1(value2);
   };
   useEffect(() => {
     applyCambio();
-  }, [value1, value2]);
+  }, [value1, value2, moeda1, moeda2]);
   const applyCambio = () => {
+    const exchangeConverter = (moeda, valor, moedaAlvo) => {
+      return Number(
+        (
+          data.valoresEmDolar[moeda] *
+          valor *
+          data.valoresNativos[moedaAlvo]
+        ).toFixed(2)
+      );
+    };
     if (lastChanged == 0) {
-      setValue2(value1 * 7);
+      setValue2(exchangeConverter(moeda1, value1, moeda2));
     }
     if (lastChanged == 1) {
-      setValue1(value2 / 7);
+      setValue1(exchangeConverter(moeda2, value2, moeda1));
     }
   };
   return (
@@ -55,6 +82,7 @@ function App() {
           style={{ display: "flex", justifyContent: "center" }}
         >
           <select
+            value={moeda1}
             name=''
             id='moeda1'
             onChange={(e) => {
@@ -67,11 +95,18 @@ function App() {
             <option value='yen'>yen</option>
           </select>
           <div className='div'>
-            <a onClick={handleInversion} href=''>
+            <a onClick={(e) => handleInversion(e)} href=''>
               <TiArrowRepeat style={{ fontSize: "2rem" }} />
             </a>
           </div>
-          <select name='' id=''>
+          <select
+            value={moeda2}
+            name=''
+            id='moeda2'
+            onChange={(e) => {
+              handleMoedaChange(e, moeda2);
+            }}
+          >
             <option value='real'>real</option>
             <option value='dolar'>dolar</option>
             <option value='euro'>euro</option>
